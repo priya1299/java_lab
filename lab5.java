dc85466827293c9bcbcea3beec3f47a6ae056c3e
priya1299 <95852555+priya1299@users.noreply.github.com>
2022-05-12 00:54:08 +0530

    Update lab5.java

--- lab5.java
+++ lab5.java
@@ -1,245 +1,420 @@
 import java.util.Scanner;
 
-public class Lab3 
+ 
+import java.util.*;
+
+ 
+public class lab5
 {
-    public static void main(String args[])
-    {
-        int choice, cont = 1;
-        Scanner sc = new Scanner(System.in);
-        while(cont == 1)
-        {
-            System.out.println("Press\n1.\tUser\n2.\tAdmin\nEnter your choice : ");
-            choice = sc.nextInt();
+  
+public static int NewRandom (int min, int max)
+  {
+    
+Random rand = new Random ();
+    
+int randomNum = rand.nextInt ((max - min) + 1) + min;
+    
+return randomNum;
+  
+}
+  
+public static void main (String[]args) 
+  {
+    
+ 
+ 
+ 
+if (args.length > 0)
+      
+ 
+      {
+	
+ 
+try 
+	{
+	  
+ 
+ 
+Scanner keyboard = new Scanner (System.in);
+	  
+ 
+ 
+ 
+System.out.println ("WELCOME TO THE BANK OF PG.");
+	  
+ 
+ 
+int firstNum = Integer.parseInt (args[0]);
+	  
+ 
+int secNum = Integer.parseInt (args[1]);
+	  
+ 
+ 
+int pin = 12345;
+	  
+int tries = 0;
+	  
+ 
+while (firstNum != pin && tries < 3)
+	    
+ 
+	    {
+	      
+ 
+System.out.println ("\nINCORRECT PIN. TRY AGAIN.");
+	      
+ 
+System.out.print ("ENTER YOUR PIN: ");
+	      
+ 
+firstNum = keyboard.nextInt ();
+	      
+tries++;
+	    
+ 
+ 
+}
+	  
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+System.out.
+	    println ("\nPIN ACCEPTED. YOU NOW HAVE ACCESS TO YOUR ACCOUNT.");
+	  
+ 
+Random rand = new Random ();
+	  
+String card = "PG";
+	  
+for (int i = 0; i < 8; i++)
+	    {
+	      
+int n = rand.nextInt (10) + 0;
+	      
+card += Integer.toString (n);
+	  
+} 
+for (int i = 0; i < 8; i++)
+	    {
+	      
+if (i % 4 == 0)
+		
+System.out.print (" ");
+	    
+ 
+}
+	  
+ 
+ 
+System.out.
+	    println ("\n This is your System Generated Session Number: " +
+		     card + secNum);
+	  
+System.out.
+	    println ("\n This is your 4 digit Session Key Entered BY you : " +
+		     secNum);
+	  
+	    /////////////////////////////////////////////////////////////////////////////////////////
+	    
+Scanner KB = new Scanner (System.in);
+	  
+ 
+	    //create initial accounts
+	    System.out.print ("How Many Customer U Want to Input : ");
+	  
+int n = KB.nextInt ();
+	  
+BankDetails C[] = new BankDetails[n];
+	  
+for (int i = 0; i < C.length; i++)
+	    {
+	      
+C[i] = new BankDetails ();
+	      
+C[i].openAccount ();
+	    
+} 
+ 
+	    //run loop until menu 5 is not pressed
+	  int ch;
+	  
+	  do
+	    {
+	      
+System.out.
+		println
+		("Main Menu\n1. Display All\n 2. Search By Account\n 3. Deposit\n 4. Withdrawal\n 5.E xit ");
+	      
+System.out.println ("Ur Choice :");
+	      ch = KB.nextInt ();
+	      
+switch (ch)
+		{
+		
+case 1:
+		  
+for (int i = 0; i < C.length; i++)
+		    {
+		      
+C[i].showAccount ();
+		    
+} 
+break;
+		
+ 
+case 2:
+		  
+System.out.
+		    print ("Enter Account No U Want to Search...: ");
+		  
+String acn = KB.next ();
+		  
+boolean found = false;
+		  
+for (int i = 0; i < C.length; i++)
+		    {
+		      
+found = C[i].search (acn);
+		      
+if (found)
+			{
+			  
+break;
+			
+}
+		    
+}
+		  
+if (!found)
+		    {
+		      
+System.out.
+			println ("Search Failed..Account Not Exist..");
+		    
+}
+		  
+break;
+		
+ 
+case 3:
+		  
+System.out.print ("Enter Account No : ");
+		  
+acn = KB.next ();
+		  
+found = false;
+		  
+for (int i = 0; i < C.length; i++)
+		    {
+		      
+found = C[i].search (acn);
+		      
+if (found)
+			{
+			  
+C[i].deposit ();
+			  
+break;
+			
+}
+		    
+}
+		  
+if (!found)
+		    {
+		      
+System.out.
+			println ("Search Failed..Account Not Exist..");
+		    
+}
+		  
+break;
+		
+ 
+case 4:
+		  
+System.out.print ("Enter Account No : ");
+		  
+acn = KB.next ();
+		  
+found = false;
+		  
+for (int i = 0; i < C.length; i++)
+		    {
+		      
+found = C[i].search (acn);
+		      
+if (found)
+			{
+			  
+C[i].withdrawal ();
+			  
+break;
+			
+}
+		    
+}
+		  
+if (!found)
+		    {
+		      
+System.out.
+			println ("Search Failed..Account Not Exist..");
+		    
+}
+		  
+break;
+		
+ 
+case 5:
+		  
+System.out.println ("Good Bye..");
+		  
+break;
+		
+}
+	    
+}
+	  
+while (ch != 5);
+	}
+	
+ 
+catch (NumberFormatException nfe) 
+	{
+	  
+ 
+System.out.println ("Argument/s must be the integer value");
+	
+ 
+}
+      
+ 
+}
+  
+ 
+}
 
-            switch(choice)
-            {
-                case 1:
-                    //multiple object of one type
-                    User user1 = new User(),user2 = new User();
-                    //array of objects
-                    User users[] = new User[3];
-                    User users[0] = new User()
-                    user1.display();
-                    
-                    break;
-                // case 2:
-                //     Admin admin1 = new Admin();
-                //     Admin admin2 = admin1;
-                //     admin1.display();
-                //     break;
-                default:
-                    System.out.println("Wrong choice!");
-                    break;
-
-            }
-
-            System.out.println("Do you want to cont?(1/0)");
-            cont = sc.nextInt();
-        }
-
-    }
+ 
 }
 
 
-class User
+ 
+class BankDetails
 {
-    Scanner sc = new Scanner(System.in);
-    void display()
-    {
-        int choice;
-        // Scanner sc = new Scanner(System.in);
-        // System.out.println("This is display class for user");
-        System.out.println("\n-----------------------------------------------------------------------------\n");
-        System.out.println("Press \n1.\tEmployee\n2.\tCustomer");
-        choice = sc.nextInt();
-        switch(choice)
-        {
-            case 1:
-            int c =1 ;
-            while(c ==1 )
-            {
-                //reference object
-                Employee emp = new Employee();
-                //anonymous object
-                new Employee().display();
-                // emp.display();
-                //anonymous class
-                Employee emp1 = new Employee("Priya","1234"){
-                    public void display(){
-                        System.out.println("Employee name : "+emp1.empName);
-                        System.out.println("Employee password : "+emp1.empPwd);
-                    }
-                }
-                emp1.display();
-                System.out.println("-------------------------------------------");
-                System.out.println("Do you want to cont with Employee id (1/0)");
-                c = sc.nextInt();
-                System.out.println("Do you want to open account?(y/n) : ");
-                String ch = sc.nextLine();
-                if(ch == "y"){
-                    User.openAccount();
-                }
-            }
-            
-            break;
-
-            case 2:
-            // Customer cust = new Customer();
-            // cust.display();
-                System.out.println("Do you want to open account?(y/n) : ");
-                String ch = sc.nextLine();
-                if(ch == "y"){
-                    User.openAccount();
-                }
-            break;
-
-            default:
-            System.out.println("Wrong choice!");
-            break;
-        }
-    }
-
-    static void openAccount(){
-        System.out.println("\nEnter name : ");
-        String user = sc.nextLine();
-        System.out.println("\nEnter date of birth : ");
-        String dob = sc.nextLine();
-        System.out.println("\nEnter pancard number : ");
-        String pan = sc.nextLine();
-        System.out.println("\nEnter aadhar card number : ");
-        String aadhar = sc.nextLine();
-        System.out.println("\nEnter address : ");
-        String address = sc.nextLine();
-        System.out.println(user+", your account has been created.");
-    }
+  
+private String accno;
+  
+private String name;
+  
+private String acc_type;
+  
+private long balance;
+  
+Scanner sc = new Scanner (System.in);
+  
+    //method to open new account  
+  public void openAccount ()
+  {
+    
+System.out.print ("Enter Account No: ");
+    
+accno = sc.next ();
+    
+System.out.print ("Enter Account type: ");
+    
+acc_type = sc.next ();
+    
+System.out.print ("Enter Name: ");
+    
+name = sc.next ();
+    
+System.out.print ("Enter Balance: ");
+    
+balance = sc.nextLong ();
+  
+} 
+    //method to display account details  
+  public void showAccount ()
+  {
+    
+ 
+ 
+System.out.println ("Name of account holder: " + name);
+    
+System.out.println ("Account no.: " + accno);
+    
+System.out.println ("Account type: " + acc_type);
+    
+System.out.println ("Balance: " + balance);
+  
+} 
+    //method to deposit money  
+  public void deposit ()
+  {
+    
+long amt;
+    
+System.out.println ("Enter the amount you want to deposit: ");
+    
+amt = sc.nextLong ();
+    
+ 
+balance = balance + amt;
+  
+} 
+    //method to withdraw money  
+  public void withdrawal ()
+  {
+    
+long amt;
+    
+System.out.println ("Enter the amount you want to withdraw: ");
+    
+amt = sc.nextLong ();
+    
+ 
+if (this.balance >= amt)
+      {
+	balance = balance - amt;
+	
+System.out.println ("Balance after withdrawal: " + balance);
+      }
+    
+    else
+      {
+	
+System.out.println ("Your balance is less than " + amt +
+			     "\tTransaction failed...!!");
+      
 }
-
-
-
-class Employee extends User
-{
-    //ststic variable
-    static int uniqEmpId;
-    Scanner sc = new Scanner(System.in);
-    int choice,empId;
-    String empName, empPwd;
-
-    static{
-        uniqEmpId=1;
-    }
-
-    //constructor overloading
-    Employee(){}
-
-    Employee(String name,String pswd){
-        this.empId = uniqEmpId++;
-        this.empName = name;
-        this.empPwd = pswd;
-    }
-
-    void display()
-    {
-        System.out.println("Enter your name : ");
-        empName = sc.nextLine();
-
-        System.out.println("Enter your password : ");
-        empPwd = sc.nextLine();
-
-        // System.out.println(empName + " "+ empPwd);
-        verify(empName, empPwd);
-    }
-
-
-
-    void verify(String name, String pwd)
-    {
-        System.out.println(name + " "+pwd);
-        if(name.equals("employee") && pwd.equals("emp123"))
-        {
-            System.out.println("Verified employee "+name);
-            System.out.println("--------------------Access granted!-------------------------------\n");
-            System.out.println("\n1.\tAdd customer\n2.\tSearch customer\nEnter choice : ");
-            choice = sc.nextInt();
-            if(choice == 1)
-            {
-                addCustomer();
-            }
-
-            else if(choice == 2)
-            {
-                String uname, id;
-                sc.nextLine();
-                System.out.println("Enter name : ");
-                uname = sc.nextLine();
-
-                System.out.println("Enter id: ");
-                id = sc.nextLine();
-
-                displayCustomerDetails(uname, id);
-
-            }
-            else
-            {
-                System.out.println("Wrong choice!");
-            }
-            
-        }
-        else
-        {
-            System.out.println("Not verified yet!");
-        }
-    }
-
-    void addCustomer()
-    {
-        String customerName, custId;
-
-        sc.nextLine();
-        System.out.println("Enter customer Name : ");
-        customerName = sc.nextLine();
-        this.empName = customerName;
-
-        System.out.println("Enter customer Id : ");
-        custId = sc.nextLine();
-        this.empId = custId;
-
-        System.out.println("\nCustomer added! with \nName : "+customerName+"\nId : "+custId);
-        // displayCustomerDetails(customerName, custId);
-    }
-
-    void changeCustomer(String name,int id){
-        this.empName = name;
-        this.empId = id;
-        System.out.println("Name and id updated");
-    }
-
-    void changeCustomer(String name){
-        this.empName = name;
-        System.out.println("Name updated");
-    }
-
-    void changeCustomer(int id){
-        this.empId = id;
-        System.out.println("Id updated");
-    }
-
-
-    void displayCustomerDetails(String name, String id)
-    {
-        String address = "", phoneNum;
-        if(name.equals("customer1") && id.equals("1"))
-        {
-            System.out.println("Person found!");
-            name = "Priya Gupta";
-            phoneNum = "9068661548";
-            address = "Delhi";
-            System.out.println("Name : "+name+"\nId : "+id+"\nAddress : "+address+"\nContact : "+phoneNum);
-        }
-        else
-        {
-            System.out.println("Person not found with : ");
-            System.out.println("Name : "+name+"\nId :"+id);
-        }
-    }
+  
+}
+  
+    //method to search an account number  
+    public boolean search (String ac_no)
+  {
+    
+ 
+if (this.accno.equals (this.accno))
+      
+ 
+      {
+	
+showAccount ();
+	
+return (true);
+      
+}
+    
+return (false);
+  
+}
 
 }
